8b32c9ce8052bf063d80a24943a37cfda96e9d2f
AKSHIT-VIG <avig4505@gmail.com>
2026-08-26 03:15:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // Stub optional deps from @coinbase/cdp-sdk that aren't published.
+    // We don't use the Base Account flow, so these code paths never execute.
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@x402/evm': false,
+      '@x402/svm/exact/client': false,
+      '@x402/evm/upto/client': false,
+      '@x402/evm/exact/client': false,
+      '@x402/core/client': false,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
